471bf1a8992ae4f577cfe76c7564a36f3314aa56
green-yellw <jack-g@sky.com>
2025-06-18 11:47:52 +0100

    add space before colon in typing for volume var
    also adjusted whitespace in funcs

--- Godot/Singletons/Settings/settings.gd
+++ Godot/Singletons/Settings/settings.gd
@@ -4,13 +4,13 @@ extends Node
 var config = ConfigFile.new()
 
 # audio
-var volume: float = 75
+var volume : float = 75
 
 
 func _ready():
 	# check if the cfg will load
 	var err = config.load("user://settings.cfg")
-	
+
 	# if it loads, set each setting to the value from the cfg
 	# (or the default if it isnt set in the cfg) - this is also where the
 	# fmod volume is actually set, but if we think its simpler to set most
@@ -20,7 +20,7 @@ func _ready():
 	if err == OK:
 		volume = config.get_value("audio", "volume", 75)
 		set_volume(volume)
-	
+
 	# if it doesnt load, save a new cfg with the current settings
 	else:
 		save_settings()
