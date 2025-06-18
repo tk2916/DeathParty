f2516035865012d3f2ead36e40588b64c7cf8a43
green-yellw <jack-g@sky.com>
2025-06-18 02:59:49 +0100

    add settings page with volume slider
    (not functional yet)

--- Godot/Utilities/pause_menu.gd
+++ Godot/Utilities/pause_menu.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 
+
 @onready var main_pause_menu: VBoxContainer = $MarginContainer/MainPauseMenu
+
+@onready var settings_menu: VBoxContainer = $MarginContainer/SettingsMenu
+@onready var volume_slider: HSlider = $MarginContainer/SettingsMenu/GridContainer/VolumeSlider
+@onready var volume_number: Label = $MarginContainer/SettingsMenu/GridContainer/VolumeNumber
+
 @onready var quit_menu: VBoxContainer = $MarginContainer/QuitMenu
 
 
@@ -8,7 +14,8 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		if main_pause_menu.visible:
 			toggle_pause()
-		elif quit_menu.visible:
+		elif quit_menu.visible or settings_menu.visible:
+			settings_menu.hide()
 			quit_menu.hide()
 			main_pause_menu.show()
 
@@ -26,6 +33,24 @@ func toggle_pause():
 
 func _on_resume_button_pressed() -> void:
 	toggle_pause()
+
+
+func _on_settings_button_pressed() -> void:
+	main_pause_menu.hide()
+	settings_menu.show()
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	volume_number.text = str(volume_slider.value)
+
+
+func _on_volume_slider_drag_ended(value_changed: bool) -> void:
+	print(volume_slider.value)
+
+
+func _on_settings_back_button_pressed() -> void:
+	settings_menu.hide()
+	main_pause_menu.show()
 
 
 func _on_quit_button_pressed() -> void:
