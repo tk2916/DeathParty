b17433316754a9d6a8e7fb004658904c3407583f
Sean Williams <seasev@users.noreply.github.com>
2025-06-19 01:57:20 -0400

    Add static type for dialogue_box_properties

--- Godot/game_logic.gd
+++ Godot/game_logic.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var json_file : JSON
-@export var message_box : Control
+@export var message_box : DialogueBoxProperties
 	
 func _ready() -> void:
 	print("Message box: ", message_box, " ", message_box.resource_file)
