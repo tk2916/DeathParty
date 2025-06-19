848c8ab583be1b206df680686ae8f53f28dc8103
Kaley Akemann <strongfish91111@gmail.com>
2025-06-18 21:50:44 -0700

    delete comments

--- Godot/Assets/GUIPrefabs/ChoicePrefabs/choice_button_script_expand.gd
+++ Godot/Assets/GUIPrefabs/ChoicePrefabs/choice_button_script_expand.gd
@@ -2,10 +2,3 @@ extends "res://Assets/GUIPrefabs/ChoicePrefabs/choice_button_script.gd"
 
 func _ready() -> void:
 	ResizableControl.new(self, text_label)
-
-#func _process(delta):
-	#var content_height : int = text_label.get_content_height()
-	#var content_width : int = text_label.get_content_width()
-	#
-	#self.custom_minimum_size.y = content_height
-	#self.custom_minimum_size.x = content_width
--- Godot/Assets/GUIPrefabs/DialogueLinePrefabs/dialogue_line_script_expand.gd
+++ Godot/Assets/GUIPrefabs/DialogueLinePrefabs/dialogue_line_script_expand.gd
@@ -2,11 +2,3 @@ extends "res://Assets/GUIPrefabs/DialogueLinePrefabs/dialogue_line_script.gd"
 
 func _ready() -> void:
 	ResizableControl.new(self, Text)
-
-#func _process(delta):
-	#var content_height : int = Text.get_content_height()
-	#var content_width : int = Text.get_content_width()
-	#
-	#self.custom_minimum_size.y = content_height
-	#self.custom_minimum_size.x = content_height
-		
