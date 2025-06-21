2d19d53cde6329bce9c2927c336d32dd39a97fad
Pedro P. <pedroaperezdr@gmail.com>
2025-06-21 17:07:12 -0500

    Removed Extra Object Viewer

--- Godot/Common/InventoryUI/Object Viewer/object_viewer.gd
+++ Godot/Common/InventoryUI/Object Viewer/object_viewer.gd
@@ -4,12 +4,26 @@ extends Control
 @export var item : MeshInstance3D
 var pressed : bool = false
 
+@onready var sub_viewport: SubViewport = $SubViewportContainer/SubViewport
+@onready var node_3d: Node3D = $SubViewportContainer/SubViewport/Node3D
+@onready var camera_3d: Camera3D = $SubViewportContainer/SubViewport/Camera3D
+
+
 #Places the item into the viewport and defines the "item" variable
 func set_item(_item : MeshInstance3D):
 	item = _item
 	
 	pass
 
+#TODO
+#At origin, the object interferes with the world map. Need to move it away from the world map so that it's visible properly.
+#Also need to assign object based on "item" variable not hard coded. 
+func _ready():
+	node_3d.transform.origin.y = node_3d.transform.origin.y - 1000
+	node_3d.transform.origin.z = node_3d.transform.origin.z - 1000
+	camera_3d.transform.origin.y = camera_3d.transform.origin.y - 1000
+	camera_3d.transform.origin.z = camera_3d.transform.origin.z - 1000
+	
 #Resets the position of the item
 func reset_item_position():
 	item.rotation.x = 0
