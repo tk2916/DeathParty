4f28e28f3a6f6878cf021d58e8888fc5ab445c8c
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-06-29 13:19:26 +0100

    fix static typing warnings

--- Godot/Singletons/settings.gd
+++ Godot/Singletons/settings.gd
@@ -4,7 +4,7 @@
 extends Node
 
 
-var config = ConfigFile.new()
+var config : ConfigFile = ConfigFile.new()
 
 # audio
 var volume : float = 50
@@ -13,9 +13,9 @@ var volume : float = 50
 var fullscreen : bool = false
 
 
-func _ready():
+func _ready() -> void:
 	# check if the cfg will load
-	var err = config.load("user://settings.cfg")
+	var err : Error = config.load("user://settings.cfg")
 	print("loading settings.cfg . . .")
 
 	# if it loads, apply each setting with the values from the cfg
@@ -40,7 +40,7 @@ func _ready():
 		save_settings()
 
 
-func save_settings():
+func save_settings() -> void:
 	# audio
 	config.set_value("audio", "volume", volume)
 
@@ -50,7 +50,7 @@ func save_settings():
 	config.save("user://settings.cfg")
 
 
-func apply_volume(value : float):
+func apply_volume(value : float) -> void:
 	var bus : FmodBus = FmodServer.get_bus("bus:/")
 	
 	# i think the volume for the bus goes from 0 to 1, so im dividing the
@@ -58,20 +58,20 @@ func apply_volume(value : float):
 	bus.set_volume(value / 100)
 
 
-func set_volume(value : float):
+func set_volume(value : float) -> void:
 	volume = value
 	apply_volume(value)
 	save_settings()
 
 
-func apply_fullscreen(enabled : bool):
+func apply_fullscreen(enabled : bool) -> void:
 	if enabled:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 
-func set_fullscreen(enabled : bool):
+func set_fullscreen(enabled : bool) -> void:
 	fullscreen = enabled
 	apply_fullscreen(enabled)
 	save_settings()
