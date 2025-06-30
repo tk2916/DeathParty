018fbf787b831ba56e91f9fd59307539595f557a
green-yellw <jack-g@sky.com>
2025-06-30 20:23:27 +0100

    rename vars and funcs to snake case

--- Godot/input_rebinding_menu.gd
+++ Godot/input_rebinding_menu.gd
@@ -1,9 +1,9 @@
 extends Control
 
 @onready var list = %BindingList
-@onready var bindingItemPrefab = preload("res://input_binding_item.tscn")
+@onready var binding_item_prefab = preload("res://input_binding_item.tscn")
 
-@onready var editableInputs = {
+@onready var editable_inputs = {
 	"move_left" : "Left",
 	"move_right" : "Right",
 	"move_up" : "Up",
@@ -13,72 +13,72 @@ extends Control
 	
 }
 
-var buttonToChange 
-var actionInputToChange
-var changingInput = false
+var button_to_change 
+var action_input_to_change
+var changing_input = false
 
 
 func _ready() -> void:
-	PopulateList()
+	populate_list()
 
 
-func PopulateList() -> void:
+func populate_list() -> void:
 	InputMap.load_from_project_settings()
 	for action in InputMap.get_actions():
-		if !editableInputs.has(action):
+		if !editable_inputs.has(action):
 			continue
-		var bindingItem = bindingItemPrefab.instantiate()
-		var labelAction = bindingItem.find_child("ActionName")
-		var inputA = bindingItem.find_child("InputA")
-		var inputB = bindingItem.find_child("InputB")
-		labelAction.text = editableInputs[action]
+		var binding_item = binding_item_prefab.instantiate()
+		var label_action = binding_item.find_child("ActionName")
+		var input_a = binding_item.find_child("InputA")
+		var input_b = binding_item.find_child("InputB")
+		label_action.text = editable_inputs[action]
 		var inputs = InputMap.action_get_events(action)
 
 		if inputs.size() == 0:
-			inputA.text = "-"
-			inputB.text = "-"
+			input_a.text = "-"
+			input_b.text = "-"
 		elif inputs.size() == 1:
 			if inputs.size() == 1:
-				inputA.text = inputs[0].as_text().trim_suffix(" (Physical)")
-				inputB.text = "-"
+				input_a.text = inputs[0].as_text().trim_suffix(" (Physical)")
+				input_b.text = "-"
 		elif inputs.size() > 1:
-			inputA.text = inputs[0].as_text().trim_suffix(" (Physical)")
-			inputB.text = inputs[1].as_text().trim_suffix(" (Physical)")
+			input_a.text = inputs[0].as_text().trim_suffix(" (Physical)")
+			input_b.text = inputs[1].as_text().trim_suffix(" (Physical)")
 
-		list.add_child(bindingItem)
-		inputA.pressed.connect(ButtonPressed.bind(inputA,action,0))
-		inputB.pressed.connect(ButtonPressed.bind(inputB,action,1))
+		list.add_child(binding_item)
+		input_a.pressed.connect(button_pressed.bind(input_a,action,0))
+		input_b.pressed.connect(button_pressed.bind(input_b,action,1))
 
 
-func ButtonPressed(inputButton,action,index) -> void:
+func button_pressed(input_button,action,index) -> void:
 	if Input.is_action_just_released("remove_input"):
-		RemoveInput(action,index)
-		inputButton.text = "-"
+		remove_input(action,index)
+		input_button.text = "-"
 	else:
-		buttonToChange = inputButton
-		actionInputToChange = InputMap.action_get_events(action)[index]
-		changingInput = true
+		button_to_change = input_button
+		action_input_to_change = InputMap.action_get_events(action)[index]
+		changing_input = true
 
 
-func RemoveInput(action,index) -> void:
+func remove_input(action,index) -> void:
 	InputMap.action_get_events(action)[index].physical_keycode = Key.KEY_NONE
 	InputMap.action_get_events(action)[index].keycode = Key.KEY_NONE
 
 
-func AddInput(action,index) -> void:
-	var actionInputs = InputMap.action_get_events(action)[index]
-	#if(actionInputs.size > index):
+func add_input(action,index) -> void:
+	var action_inputs = InputMap.action_get_events(action)[index]
+	#if(action_inputs.size > index):
 		
 	#else:
 		
 
 
 func _input(event) -> void:
-	if changingInput:
+	if changing_input:
 		if(event is InputEventKey):
-			buttonToChange.text = event.as_text_keycode()
-			actionInputToChange.keycode = event.keycode
-			actionInputToChange.physical_keycode = event.keycode
-			buttonToChange = null
-			actionInputToChange = null
-			changingInput = false
+			button_to_change.text = event.as_text_keycode()
+			action_input_to_change.keycode = event.keycode
+			action_input_to_change.physical_keycode = event.keycode
+			button_to_change = null
+			action_input_to_change = null
+			changing_input = false
