0417236c3ffd29a40f238479d837b8e72f86ced1
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-07-02 23:17:52 +0100

    add triggers for sound in button script
    (these should maybe be somewhere else down the line if we want them to play when journal is opened thru a hotkey)

--- Godot/journal_open_button.gd
+++ Godot/journal_open_button.gd
@@ -18,8 +18,10 @@ func _pressed() -> void:
 		#model_holder.add_child(journal.instantiate())
 		#object_viewer.camera_3d.projection = Camera3D.PROJECTION_ORTHOGONAL
 		#object_viewer.camera_3d.size = 2.1
+		journal_open_sound.play()
 		object_viewer.visible = true
 	else:
+		journal_close_sound.play()
 		object_viewer.visible = false
 		object_viewer.camera_3d.projection = Camera3D.PROJECTION_PERSPECTIVE
 		object_viewer.remove_current_item()
