5ddd4299ae3c1572b2d8ad16c2fdc6f6c44f975d
Chrisjoi503 <christinajou125@gmail.com>
2025-07-05 15:08:07 -0700

    Added polaroid camera mechanic to the game
    When player clicks the question mark, hinting to player to take a picture, player goes into picture taking mode and can move the viewfinder around an image and shoot. When the player shoots (via mouse click) the player exits out of picture taking mode and back to main scene.

---
+++ Godot/polaroid_layer.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+var screen_size 
+var Picture
+var timer=Timer.new()
+
+	
+func _process(delta):
+	Picture=$PictureExample/ViewFinder
+	var velocity = Vector2.ZERO 
+	if Input.is_action_pressed("move_right"):
+		Picture.position.x += 1.5
+	if Input.is_action_pressed("move_left"):
+		Picture.position.x -= 1.5
+	if Input.is_action_pressed("move_down"):
+		Picture.position.y += 5
+	if Input.is_action_pressed("move_up"):
+		Picture.position.y -= 5
+		
+	if	$PictureExample/ViewFinder.position.x < 0:
+		$PictureExample/ViewFinder.position.x = 0
+	if	$PictureExample/ViewFinder.position.x > 656:
+		$PictureExample/ViewFinder.position.x = 656
+	if	$PictureExample/ViewFinder.position.y < 0:
+		$PictureExample/ViewFinder.position.y = 0
+	if	$PictureExample/ViewFinder.position.y > 328:
+		$PictureExample/ViewFinder.position.y = 328
+	
+
+
+
+func _on_shoot_pressed() -> void:
+	visible=false
+
+
+func _on_questionbutton_pressed() -> void:
+	visible=true
---
+++ Godot/question_mark.gd
@@ -0,0 +1,7 @@
+extends TextureRect
+
+
+
+
+func _on_questionbutton_pressed() -> void:
+	pass # Replace with function body.
