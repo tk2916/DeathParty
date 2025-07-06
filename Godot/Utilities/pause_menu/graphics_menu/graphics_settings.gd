99e93f239ecd2b13788c40e8f05223a5792585aa
Sean Williams <seasev@users.noreply.github.com>
2025-07-06 01:58:11 -0400

    Add monitor selection setting

--- Godot/Utilities/pause_menu/graphics_menu/graphics_settings.gd
+++ Godot/Utilities/pause_menu/graphics_menu/graphics_settings.gd
@@ -2,11 +2,40 @@ extends Control
 
 
 @onready var fullscreen_option_button : OptionButton = %FullscreenOptionButton
+@onready var monitor_option_button : OptionButton = %MonitorOptionButton
+
+var last_monitor_count : int
 
 
 func _ready() -> void:
 	fullscreen_option_button.selected = Settings.fullscreen
+	
+	set_monitor_options()
+
+func _process(_delta: float) -> void:
+	if DisplayServer.get_screen_count() != last_monitor_count:
+		set_monitor_options()
+
+
+func set_monitor_options() -> void:
+	monitor_option_button.clear()
+	
+	last_monitor_count = DisplayServer.get_screen_count()
+	var select_i = 0
+	for i in range(last_monitor_count):
+		var is_current := ""
+		if i == DisplayServer.window_get_current_screen():
+			is_current = " (Current)"
+			select_i = i
+		monitor_option_button.add_item("Monitor %s%s" % [i,is_current])
+	
+	monitor_option_button.select(select_i)
 
 
 func _on_fullscreen_option_button_item_selected(value : int) -> void:
 	Settings.set_fullscreen(value)
+
+
+func _on_monitor_option_button_item_selected(index: int) -> void:
+	DisplayServer.window_set_current_screen(index)
+	set_monitor_options()
