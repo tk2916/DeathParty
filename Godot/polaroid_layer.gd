95c0803d39b8a142bc99881f9bb516239bd2ce11
Chrisjoi503 <christinajou125@gmail.com>
2025-07-06 10:08:29 -0700

    Added polaroid camera to main scene and a question mark in 3d world to indicate player to take a picture

--- Godot/polaroid_layer.gd
+++ Godot/polaroid_layer.gd
@@ -1,9 +1,6 @@
 extends CanvasLayer
 
-var screen_size 
 var Picture
-var timer=Timer.new()
-
 	
 func _process(delta):
 	Picture=$PictureExample/ViewFinder
@@ -19,17 +16,22 @@ func _process(delta):
 		
 	if	$PictureExample/ViewFinder.position.x < 0:
 		$PictureExample/ViewFinder.position.x = 0
-	if	$PictureExample/ViewFinder.position.x > 656:
-		$PictureExample/ViewFinder.position.x = 656
+	if	$PictureExample/ViewFinder.position.x > 100:
+		$PictureExample/ViewFinder.position.x = 100
 	if	$PictureExample/ViewFinder.position.y < 0:
 		$PictureExample/ViewFinder.position.y = 0
-	if	$PictureExample/ViewFinder.position.y > 328:
-		$PictureExample/ViewFinder.position.y = 328
+	if	$PictureExample/ViewFinder.position.y > 142:
+		$PictureExample/ViewFinder.position.y = 142
 
-#when picture indicator is pressed, player enters polaroid scene
-func _on_questionbutton_pressed() -> void:
-	visible=true	
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		print("interact")
+		$PictureExample/ViewFinder/Camera2D.enabled=true
+		$PictureExample/ViewFinder/Camera2D.make_current()
+		visible=true	
 #when player shoots the picture, the image dissapears 
-func _on_shoot_pressed() -> void:
+func _on_shoot_button_up() -> void:
 	visible=false
-	
+	$PictureExample/ViewFinder/Camera2D.enabled=false
+
+#when question mark is pressed, player enters polaroid scene
