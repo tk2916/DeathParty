61dd2aec949948478d967094cec65fde8e5d5200
Kaley Akemann <strongfish91111@gmail.com>
2025-07-07 20:12:39 -0700

    player in range detection

--- Godot/Entities/EntityScripts/npc.gd
+++ Godot/Entities/EntityScripts/npc.gd
@@ -1,8 +1,12 @@
-extends StaticBody3D
+extends Interactable
+
+var outline : Node3D
 
 func _ready() -> void:
 	$InteractionDetector.player_interacted.connect(when_interacted)
-
+	outline = get_node_or_null("Outline")
+	if outline:
+		outline.visible = false
 
 func when_interacted(_body : Node3D) -> void:
 	print("interacted")
