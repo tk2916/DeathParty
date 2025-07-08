786aad9843cc3476b2e42e508be11f8d627207b1
Kaley Akemann <strongfish91111@gmail.com>
2025-07-08 00:12:27 -0700

    trying to make labels not blurry

--- Godot/Assets/models/journal_tab_script.gd
+++ Godot/Assets/models/journal_tab_script.gd
@@ -4,11 +4,13 @@ class_name JournalTab
 @export var text : String
 @export var color : Color
 @export var offset_distance : float = .2
+var og_rotation : Vector3
+var default_rotation : Vector3 = Vector3(0,deg_to_rad(-180),0)
 
 @onready var up_direction : Vector3
 
 var sub_viewport : Viewport
-var button : Button
+var label : RichTextLabel
 
 @export var flip_to_page : int
 
@@ -18,23 +20,25 @@ signal tab_pressed
 
 func _ready() -> void:
 	sub_viewport = $Tab/SubViewport
-	button = sub_viewport.get_node("Button")
-	button.text = text
+	label = sub_viewport.get_node("RichTextLabel")
+	label.text = text
 	#sub_viewport.pressed.connect(button_pressed)
 	$Cube.get_surface_override_material(0).albedo_color = color
 	sub_viewport.get_node("ColorRect").color = color
+	og_rotation = rotation
+	rotation = default_rotation
 
 func return_to_original_pos():
 	up_direction = -transform.basis.z.normalized()
 	var offset = up_direction*offset_distance
-	print("Moving down")
-	global_position = global_position - offset	
+	global_position = global_position - offset
+	rotation = default_rotation
 
 func move_upward():
 	up_direction = -transform.basis.z.normalized()
 	var offset = up_direction*offset_distance
-	print("Move upward")
 	global_position = global_position + offset
+	rotation = og_rotation
 
 func toggle_visible(tf : bool = !visible):
 	visible = tf
@@ -45,15 +49,14 @@ func button_pressed():
 	print(self, " Pressed!")
 	tab_pressed.emit()
 
-##INHERITED METHODS (OVERRIDE)
+##INHERITED METHODS (OVERRIDDEN)
 func enter_hover():
-	print("Entered tab hover: ", self.name)
-	#Interact.set_active_subviewport(sub_viewport)
+	pass
+	#print("Entered tab hover: ", self.name)
 
 func exit_hover():
 	pass
 	#print("Exited tab hover ", self.name)
-	#Interact.clear_active_subviewport()
 
 func on_interact():
 	button_pressed()
--- Godot/Entities/Interactable/object_viewer_interactable.gd
+++ Godot/Entities/Interactable/object_viewer_interactable.gd
@@ -1,5 +1,4 @@
-extends Node3D
-class_name ObjectViewerInteractable
+class_name ObjectViewerInteractable extends Node3D
 
 #The interactable class is any object that is interactable. This means that if the mouse hovers over it
 #it will become highlighted and the player can click on it to get some event
