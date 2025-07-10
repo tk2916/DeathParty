d3d4d4054bb4403a474a1c68f74ac0669d5a0f0c
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-07-11 00:58:06 +0100

    remove borderless window mode option
    let me know if you think we should still keep this !
    
    also we could maybe just do a tick box for fullscreen if theres only 2 options 💭

--- Godot/Singletons/settings.gd
+++ Godot/Singletons/settings.gd
@@ -186,8 +186,6 @@ func apply_fullscreen(mode : int) -> void:
 		0:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		1:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		2:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 
 
