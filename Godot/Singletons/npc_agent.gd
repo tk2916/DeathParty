79327c90de1287c0e7b6b3fdb240d7cdb85f91e4
shquam <87450533+shquam@users.noreply.github.com>
2025-07-20 13:26:28 -0400

    improve pathing and animation handling

--- Godot/Singletons/npc_agent.gd
+++ Godot/Singletons/npc_agent.gd
@@ -3,6 +3,7 @@ class_name NPCAgent
 
 @export var model: Node3D
 @export var animation_tree: AnimationTree
+@export var node_path: Path3D
 
 @onready var default_map_rid: RID = get_world_3d().get_navigation_map()
 
@@ -13,7 +14,7 @@ var blend_speed: float = 8.0
 var walk_blend: float = 0.0
 
 ## Movement path variables
-var path_point_margin: float = 0.5
+var path_point_margin: float = 1
 var current_path_index: int = 0
 var current_path_point: Vector3
 var current_path: PackedVector3Array
@@ -24,12 +25,19 @@ func set_movement_target(destination: Vector3, navigation_layers: int, map: RID 
 	var start_position: Vector3 = global_transform.origin
 	## Randomize if NPC moves optimally for variation
 	var optimize_path: bool = RandomNumberGenerator.new().randi_range(0, 1) == 1
+	#var optimize_path: bool = true
 	
 	current_path = NavigationServer3D.map_get_path(map, start_position, destination, optimize_path, navigation_layers)
+	
 	
 	if not current_path.is_empty():
 		current_path_index = 0
 		current_path_point = current_path[0]
+		
+		var curve: Curve3D = Curve3D.new()
+		for vertex: Vector3 in current_path:
+			curve.add_point(vertex)
+		node_path.set_curve(curve)
 
 
 ## Create a path to a random destination
@@ -42,6 +50,7 @@ func wander(movement_speed: float, navigation_layers: int, delta: float = 0, map
 	if not current_path.is_empty():
 		move_npc(movement_speed, delta)
 		return
+	animate_npc(delta)
 	
 	## physics_process runs 60 times per second
 	## randi from 1-200 should be a .005 chance to get '1' -> .5% chance to decide to move every frame
@@ -53,7 +62,7 @@ func wander(movement_speed: float, navigation_layers: int, delta: float = 0, map
 	set_movement_target_random(navigation_layers)
 
 ## Move agent along current path
-func move_npc(movement_speed: float, _delta: float = 0) -> void:
+func move_npc(movement_speed: float, delta: float = 0) -> void:
 	if current_path.is_empty():
 		return
 	
@@ -63,6 +72,7 @@ func move_npc(movement_speed: float, _delta: float = 0) -> void:
 			current_path = []
 			current_path_index = 0
 			current_path_point = global_transform.origin
+			velocity = Vector3.ZERO
 			return
 	
 	current_path_point = current_path[current_path_index]
@@ -74,3 +84,31 @@ func move_npc(movement_speed: float, _delta: float = 0) -> void:
 	velocity = new_velocity
 	move_and_slide()
 	apply_floor_snap() # Keep NPC on ground
+	animate_npc(delta)
+
+
+## play NPC animations
+func animate_npc(delta: float) -> void:
+	if not model or not animation_tree:
+		return
+	
+	# set anim state to IDLE if not moving
+	if velocity == Vector3.ZERO:
+		current_animation = ANIMATION_STATE.IDLE
+
+	# set anim state to WALK if moving
+	elif velocity != Vector3.ZERO:
+		current_animation = ANIMATION_STATE.WALK
+
+	# interpolate blend between IDLE and WALK anim states
+	match current_animation:
+		ANIMATION_STATE.IDLE:
+			walk_blend = lerpf(walk_blend, 0, blend_speed * delta)
+		ANIMATION_STATE.WALK:
+			walk_blend = lerpf(walk_blend, 1, blend_speed * delta)
+	
+	animation_tree["parameters/Walk Blend/blend_amount"] = walk_blend
+	
+	# Rotate model
+	if current_animation == ANIMATION_STATE.WALK:
+		model.rotation.y = lerp_angle(model.rotation.y, basis.z.signed_angle_to(velocity, basis.y), blend_speed * delta)
