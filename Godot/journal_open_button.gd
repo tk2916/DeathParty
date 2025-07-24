0eb2a30f0831a84d4a277ef1433af59a4b5c92ef
Kaley Akemann <strongfish91111@gmail.com>
2025-07-24 16:48:05 -0700

    GUI controller and gui button changes

--- Godot/journal_open_button.gd
+++ Godot/journal_open_button.gd
@@ -1,27 +1,11 @@
-extends "res://Assets/GUIDesignScripts/default_gui_button.gd"
-
-@onready var journal_open_sound : FmodEventEmitter2D = %JournalOpenSound
-@onready var journal_close_sound : FmodEventEmitter2D = %JournalCloseSound
-
-
-@export var journal_path : String
-@export var journal : PackedScene
-
-@export var object_viewer : Control
+class_name JournalOpenButton extends GuiButton
 
 var journal_showing : bool = false
 
 func _pressed() -> void:
 	print("Journal button pressed")
 	if journal_showing == false:
-		object_viewer.set_item(journal_path)
-		#object_viewer.camera_3d.projection = Camera3D.PROJECTION_ORTHOGONAL
-		#object_viewer.camera_3d.size = 2.1
-		journal_open_sound.play()
-		object_viewer.visible = true
+		gui_controller.show_journal()
 	else:
-		journal_close_sound.play()
-		object_viewer.visible = false
-		#object_viewer.camera_3d.projection = Camera3D.PROJECTION_PERSPECTIVE
-		object_viewer.remove_current_item()
+		gui_controller.hide_journal()
 	journal_showing = !journal_showing
