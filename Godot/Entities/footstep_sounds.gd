b65a5274e77a8d08d661d12e0a93f2e24114fb72
green-yellw <jack-g@sky.com>
2025-08-06 16:51:13 +0100

    add comments to script, comment print statements

--- Godot/Entities/footstep_sounds.gd
+++ Godot/Entities/footstep_sounds.gd
@@ -7,30 +7,39 @@ var previous_position: Vector3 = global_position
 var speed: Vector3
 
 
+# track position across physics frames to see if character is moving
 func _physics_process(_delta: float) -> void:
 	speed = global_position - previous_position
 	previous_position = global_position
 
 
+# this func should be called from character animations
+# on the frame before their feet touch the ground
 func play_footstep_sound() -> void:
+	# use the raycast to get the surface the character is standing on
 	var surface: Node3D = ray_cast.get_collider()
 	var surface_groups: Array
-	
+
+	# get all the groups the surface node is in
 	if surface:
-		print("surface: ", surface)
+		#print("surface: ", surface)
 		surface_groups = surface.get_groups()
-		print("surface groups: ", surface_groups)
-	
+		#print("surface groups: ", surface_groups)
+
 	var surface_material: String
 
+	# if the surface is in a group starting with "material_",
+	# store the name of the material
 	for group in surface_groups:
 		if group.begins_with("material"):
 			surface_material = group.trim_prefix("material_")
 			print("surface material: ", surface_material)
 			break
 
+	# set the fmod land texture parameter based on the surface material
 	if surface_material:
 		set_parameter("LandTexture", surface_material)
 
+	# play a step sound if the character is moving
 	if speed != Vector3.ZERO:
 		play()
