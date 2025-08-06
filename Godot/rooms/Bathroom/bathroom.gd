91e84053ce5ecb6110972eb05ffce175ea7295ed
jonasma0628 <zm2420@columbia.edu>
2025-08-06 00:20:17 -0400

    added bathroom but camera needs to be fixed

---
+++ Godot/rooms/Bathroom/bathroom.gd
@@ -0,0 +1,20 @@
+extends Room3D
+
+func _ready() -> void:
+	super()
+	GlobalPlayerScript.player_moved.connect(hide_entrance)
+	body_entered.connect(handle_player_entrance)
+
+func handle_player_entrance(body: Node3D) -> void:
+	remove_all_bounds(body)
+	rotate_player(body)
+	
+	keep_camera_on_player(body)
+	bind_camera_LR(body)
+
+
+func hide_entrance(pos: Vector3) -> void:
+	if(background_plane.distance_to(pos) < 0):
+		visible = false
+	else:
+		visible = true
