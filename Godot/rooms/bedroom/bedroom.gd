89cf8f913c2595208aa8aac28388ec37b84c433b
shquam <87450533+shquam@users.noreply.github.com>
2025-08-10 16:05:01 -0400

    start tutorial path

--- Godot/rooms/bedroom/bedroom.gd
+++ Godot/rooms/bedroom/bedroom.gd
@@ -1,14 +1,13 @@
 extends Room3D
 
-
 func _ready() -> void:
 	super()
+	GlobalPlayerScript.player_moved.connect(_calculate_progress_ratio)
 	body_entered.connect(handle_player_entrance)
-
 
 func handle_player_entrance(body: Node3D) -> void:
 	remove_all_bounds(body)
 	rotate_player(body)
+	bind_camera_path(body)
 	
-	keep_camera_on_player(body)
-	FmodServer.set_global_parameter_by_name_with_label("room", "outside")
+	#FmodServer.set_global_parameter_by_name_with_label("room", "outside")
