f236ae7d81223c8eb9605662d8b6f6b19d297db2
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-08-10 20:58:38 +0100

    make player speed button and devmenu scene

---
+++ Godot/Utilities/dev_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+@export var player: CharacterBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_toggled(toggled_on: bool) -> void:
+	pass
