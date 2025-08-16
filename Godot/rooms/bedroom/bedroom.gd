616d17a16a4e72f1a1a69b8c053aabc7b97e93d2
jonasma0628 <zm2420@columbia.edu>
2025-08-16 12:36:57 -0400

    fixed camera angle and teleport bug in library

--- Godot/rooms/bedroom/bedroom.gd
+++ Godot/rooms/bedroom/bedroom.gd
@@ -14,7 +14,7 @@ func handle_player_entrance(body: Node3D) -> void:
 	rotate_player(body)
 
 	keep_camera_on_player(body)
-	bind_camera_y(body)
+	bind_camera_y(body, 1.5, 1.5)
 
 
 func _on_scene_loader_body_entered(body: Node3D) -> void:
