e5f38272987e7189d0b9bf1946d1b1169510e6e5
shquam <87450533+shquam@users.noreply.github.com>
2025-08-17 15:34:14 -0400

    delete tutorial script

--- Godot/rooms/Tutorial/tutorial.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var main: Node = preload("res://main.tscn").instantiate()
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact"):
-		get_tree().change_scene_to_file("res://main.tscn")
-		ContentLoader.reset()
