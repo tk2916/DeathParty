8ec485841dd4df4e74f16465c1f3134fae64c287
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-08-21 13:38:35 +0100

    disable dev menu room tracking, add warning
    i was tracking the 'current room' in the dev menu script and checking if the player was already in a room when they tried tping to it (to a avoid a crash when tping to the current room) but i realised i was only setting the current room when the player tps with the dev menu so it would think you were in whichever room you last tpd to, rather than the room you were actually in lol
    
    thought this would be confusing because ppl would think the tping was randomly broken if that happened so i just disabled it for now and added a warning about tping to the current room
    
    probably fine for an internal dev menu TBH

--- Godot/Utilities/dev_menu/dev_menu.gd
+++ Godot/Utilities/dev_menu/dev_menu.gd
@@ -13,7 +13,13 @@ extends CanvasLayer
 @export var teleport_button_scene: PackedScene
 
 var player_added_speed := 10.0
-var current_room: String
+
+# var for tracking the current scene (was made to avoid a crash when trying
+# to tp to the current room, but commented it out because it could only track
+# the scene changing thru tping in the menu (and not thru the player moving
+# between rooms normally) which would probably end up being more confusing
+# than crashing lol
+#var current_room: String
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,7 +36,7 @@ func _ready() -> void:
 
 		teleport_button_container.add_child(teleport_button)
 
-		current_room = ContentLoader.og_scene
+		#current_room = ContentLoader.og_scene
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -79,6 +85,6 @@ func _on_hide_ui_button_toggled(toggled_on: bool) -> void:
 
 
 func teleport_player(room: String) -> void:
-	if room != current_room:
-		ContentLoader.direct_teleport_player(room)
-		current_room = room
+	#if room != current_room:
+	ContentLoader.direct_teleport_player(room)
+	#current_room = room
