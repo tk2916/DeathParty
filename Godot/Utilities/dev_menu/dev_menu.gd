52b49531158e16a7aef4283b272e912e05cc4817
green-yellw <jack-g@sky.com>
2025-08-21 11:09:12 +0100

    handle player trying to tp to current room
    fixes a crash

--- Godot/Utilities/dev_menu/dev_menu.gd
+++ Godot/Utilities/dev_menu/dev_menu.gd
@@ -13,6 +13,7 @@ extends CanvasLayer
 @export var teleport_button_scene: PackedScene
 
 var player_added_speed := 10.0
+var current_room: String
 
 
 # Called when the node enters the scene tree for the first time.
@@ -28,6 +29,8 @@ func _ready() -> void:
 		teleport_button.pressed.connect(func() -> void: teleport_player(teleport_button.text))
 
 		teleport_button_container.add_child(teleport_button)
+
+		current_room = ContentLoader.og_scene
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -76,4 +79,6 @@ func _on_hide_ui_button_toggled(toggled_on: bool) -> void:
 
 
 func teleport_player(room: String) -> void:
-	ContentLoader.direct_teleport_player(room)
+	if room != current_room:
+		ContentLoader.direct_teleport_player(room)
+		current_room = room
