d58c269fca14d15687ee2bb3742ecb9454903041
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-08-30 15:42:16 +0100

    add another state to tutorial to improve logic

--- Godot/Utilities/tutorial/tutorial_ui.gd
+++ Godot/Utilities/tutorial/tutorial_ui.gd
@@ -16,13 +16,14 @@ extends CanvasLayer
 @export var door: Node3D
 
 @onready var move_controls_popup: PanelContainer = %MoveControlsPopup
+@onready var walk_complete_timer: Timer = %WalkCompleteTimer
 @onready var phone_controls_popup: PanelContainer = %PhoneControlsPopup
 @onready var journal_controls_popup: PanelContainer = %JournalControlsPopup
 
 var player: Player
 
 enum States {
-	INTRO, WALK, UNLOCK_PHONE, USING_PHONE,
+	INTRO, WALK, WALK_COMPLETE, UNLOCK_PHONE, USING_PHONE,
 	OPEN_JOURNAL, USING_JOURNAL, TUTORIAL_FINISHED
 	}
 
@@ -36,6 +37,9 @@ var state: States:
 				print("TUTORIAL STEP: WALK")
 				player.movement_disabled = false
 				move_controls_popup.show()
+			States.WALK_COMPLETE:
+				print("TUTORIAL STEP: WALK COMPLETE")
+				walk_complete_timer.start()
 			States.UNLOCK_PHONE:
 				print("TUTORIAL STEP: UNLOCK PHONE")
 				move_controls_popup.hide()
@@ -54,7 +58,6 @@ var state: States:
 				door.show()
 				queue_free()
 
-var move_controls_popup_fade_timer_started := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -84,9 +87,8 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	match state:
 		States.WALK:
-			if player.player_velocity != Vector3.ZERO and not move_controls_popup_fade_timer_started:
-				$MoveControlsPopupFadeTimer.start()
-				move_controls_popup_fade_timer_started = true
+			if player.player_velocity != Vector3.ZERO:
+				state += 1
 		States.UNLOCK_PHONE:
 			if get_tree().get_first_node_in_group("phone").visible == true:
 				state += 1
@@ -105,5 +107,5 @@ func _on_bedroom_intro_finished() -> void:
 	state += 1
 
 
-func _on_move_control_popup_fade_timer_timeout() -> void:
+func _on_walk_complete_timer_timeout() -> void:
 	state += 1
