03f7df493ffe8402a3483297aef51026738ee0c9
Kaley Akemann <strongfish91111@gmail.com>
2025-08-30 16:04:59 -0700

    Added dialogue for viewing items for the first time

--- Godot/Assets/Resources/InventoryItemResources/Default Resource (DO NOT EDIT)/inventory_properties_script.gd
+++ Godot/Assets/Resources/InventoryItemResources/Default Resource (DO NOT EDIT)/inventory_properties_script.gd
@@ -5,3 +5,6 @@ class_name InventoryItemResource extends Resource
 @export var model : PackedScene
 @export var inventory_scale : float = 1.0
 @export var inventory_position : Vector2 = Vector2.ZERO
+
+@export var dialogue_on_first_view : JSON
+var viewed : bool = false
