3a895c4669d0d8238da567e1e931738b8aef59a7
green-yellw <110565845+green-yellw@users.noreply.github.com>
2025-09-06 20:39:56 +0100

    add controls and logic for jogging

--- Godot/Utilities/scripts/player.gd
+++ Godot/Utilities/scripts/player.gd
@@ -1,37 +1,40 @@
 class_name Player extends CharacterBody3D
 
-@onready var model : Node3D = %PlayerModel
-@onready var animation_tree : AnimationTree = %AnimationTree
-@onready var previous_position : Vector3 = global_position
-@onready var cell_position : Vector3 = global_position
-@onready var footstep_sounds : FmodEventEmitter3D = $FootstepSounds
-@onready var spawn_position : Vector3 = global_position
+@onready var model: Node3D = %PlayerModel
+@onready var animation_tree: AnimationTree = %AnimationTree
+@onready var previous_position: Vector3 = global_position
+@onready var cell_position: Vector3 = global_position
+@onready var footstep_sounds: FmodEventEmitter3D = $FootstepSounds
+@onready var spawn_position: Vector3 = global_position
 
 @export var player_speed := 2.3
-@export var horizontal_offset : float = 1.3
+@export var horizontal_offset: float = 1.3
 
-@export var player_camera_location : Node3D
+@export var player_camera_location: Node3D
 
-var player_velocity : Vector3 = Vector3.ZERO
-var original_camera_position : Vector3 = Vector3.ZERO
+var player_velocity: Vector3 = Vector3.ZERO
+var original_camera_position: Vector3 = Vector3.ZERO
 
 var facing: int = 0
 var movement_direction: int = 0
 var prev_movement_direction: int = 0
 
+var holding_jog_input := false
+var jog_speed_multiplier := 1.5
+
 # animation enum and vars
 enum AnimationState {IDLE, WALK, TURN} # TURN currently unused
-var blend_speed : float = 8
-var walk_blend : float = 0
-var current_animation : AnimationState = AnimationState.IDLE
+var blend_speed: float = 8
+var walk_blend: float = 0
+var current_animation: AnimationState = AnimationState.IDLE
 
-var movement_disabled : bool = false
+var movement_disabled: bool = false
 
 func _enter_tree() -> void:
 	original_camera_position = player_camera_location.position
 
 
-func _physics_process(delta : float) -> void:
+func _physics_process(delta: float) -> void:
 	if DialogueSystem.in_dialogue or GuiSystem.in_gui or movement_disabled:
 		player_velocity = Vector3.ZERO
 		handle_animations(delta)
@@ -46,9 +49,9 @@ func _physics_process(delta : float) -> void:
 		pass
 	elif Input.is_action_pressed("move_left"):
 		facing = -1
-		movement_direction_x = -basis.x
+		movement_direction_x = - basis.x
 		player_camera_location.position.x -= horizontal_offset
-	elif  Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		facing = 1
 		movement_direction_x = basis.x
 		player_camera_location.position.x += horizontal_offset
@@ -58,13 +61,16 @@ func _physics_process(delta : float) -> void:
 	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_down"):
 		pass
 	elif Input.is_action_pressed("move_up"):
-		movement_direction_z = -basis.z
+		movement_direction_z = - basis.z
 	elif Input.is_action_pressed("move_down"):
 		movement_direction_z = basis.z
 
 	# Move on x axis
 	player_velocity = movement_direction_x + movement_direction_z
 	player_velocity = player_velocity.normalized() * player_speed
+
+	if Input.is_action_pressed("jog"):
+		player_velocity *= jog_speed_multiplier
 
 	# Fall
 	if not is_on_floor():
@@ -79,10 +85,10 @@ func _physics_process(delta : float) -> void:
 	move_and_slide()
 
 	# If position changed, emit position
-	if(global_position != previous_position):
+	if (global_position != previous_position):
 		GlobalPlayerScript.player_moved.emit(global_position)
 		previous_position = global_position
-		var cell_dist : float = global_position.distance_squared_to(cell_position)
+		var cell_dist: float = global_position.distance_squared_to(cell_position)
 		#print("cell dist: ", cell_dist)
 		if (cell_dist > 6):
 			#only update active cells once player has moved > sqrt(10) units
@@ -124,9 +130,9 @@ func rotate_model(delta: float) -> void:
 	# rotate model slightly towards camera while idle
 	if current_animation == AnimationState.IDLE:
 		if facing == -1:
-			model.rotation.y = lerp_angle(model.rotation.y, -PI/5, blend_speed * delta)
+			model.rotation.y = lerp_angle(model.rotation.y, -PI / 5, blend_speed * delta)
 		if facing == 1:
-			model.rotation.y = lerp_angle(model.rotation.y, PI/5, blend_speed * delta)
+			model.rotation.y = lerp_angle(model.rotation.y, PI / 5, blend_speed * delta)
 
 	# rotate while walking
 	if current_animation == AnimationState.WALK:
@@ -139,7 +145,7 @@ func reset_position():
 	ContentLoader.reset()
 
 
-func _on_world_boundary_body_entered(body : Node3D) -> void:
+func _on_world_boundary_body_entered(body: Node3D) -> void:
 	if body == self:
 		print("player out of bounds, resetting position . . .")
 		reset_position()
