018035513fb450ea8262ab651ff3c8539da5761d
Pedro P. <pedroaperezdr@gmail.com>
2025-09-19 21:11:12 -0500

    Fixed Changes

--- Godot/Singletons/Interactable/interactable_priority.gd
+++ Godot/Singletons/Interactable/interactable_priority.gd
@@ -20,15 +20,17 @@ func add_interactable(interactable : InteractionDetector):
 	active_interactable.enable()
 
 func remove_interactable(interactable : InteractionDetector):
+	print("Remove Object")
 	#If the chosen interactable is the active one : Remove it and replace it with the next active
 	if interactable == active_interactable:
 		active_interactable.disable()
 		interactable_priority.pop_back()
 		if not interactable_priority.is_empty():
 			active_interactable = interactable_priority[-1]
-		active_interactable.enable()
+			active_interactable.enable()
 	else:
 		#Find the interactable and remove it
-		for i in range(interactable_priority.size()):
+		for i in range(interactable_priority.size()-1):
 			if interactable == interactable_priority[i]:
+				interactable.disable()
 				interactable_priority.pop_at(i)
